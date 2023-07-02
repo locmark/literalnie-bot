f6ffc1057e6d6d3a8097e00ed2d27e3a2719ccea
locmark <kkraczyna@gmail.com>
2023-07-02 23:11:04 +0200

    input

--- src/components/Letter.svelte
+++ src/components/Letter.svelte
@@ -1,15 +1,40 @@
 <script lang="ts">
-    import { LetterScore } from 'src/types.js'
     import ScoreButton from 'src/components/ScoreButton.svelte'
+    import { LetterScore } from 'src/types.js'
+    import { createEventDispatcher } from 'svelte'
+
+    const dispatch = createEventDispatcher<{
+        click: void
+        backspace: void
+    }>()
 
     export let letter: string = ''
     export let score: LetterScore = LetterScore.GRAY
+
+    function onInput(event: Event & { currentTarget: EventTarget & HTMLInputElement }) {
+        letter = (event.target as HTMLInputElement).value.toUpperCase()
+        // limit to one character
+        if (letter.length > 1) {
+            letter = letter[letter.length - 1]
+        }
+
+        // only allow letters
+        if (!letter.match(/[a-z ęóąśłżźćń]/i)) {
+            letter = ''
+        }
+
+        if (letter.length > 0) {
+            dispatch('click')
+        } else {
+            dispatch('backspace')
+        }
+    }
 </script>
 
 <div class="container">
     <ScoreButton color="#538d4e" />
     <div class="input">
-        {letter}
+        <input type="text" on:input={onInput} value={letter} />
     </div>
     <ScoreButton color="#b59f3b" />
 </div>
@@ -31,5 +56,16 @@
         display: flex;
         align-items: center;
         justify-content: center;
+    }
+
+    input {
+        width: 100%;
+        height: 100%;
+        border: none;
+        background-color: transparent;
+        text-align: center;
+        font-size: 1.5em;
+        font-weight: bold;
+        color: white;
     }
 </style>
--- src/components/Row.svelte
+++ src/components/Row.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
     import Letter from 'src/components/Letter.svelte'
+
+    export let letters: string[] = ['', '', '', '', '']
 </script>
 
 <div class="row">
-    <Letter letter="A" />
-    <Letter letter="B" />
-    <Letter letter="C" />
-    <Letter letter="D" />
-    <Letter letter="E" />
+    <Letter letter={letters[0]} />
+    <Letter letter={letters[1]} />
+    <Letter letter={letters[2]} />
+    <Letter letter={letters[3]} />
+    <Letter letter={letters[4]} />
 </div>
 
 <style lang="scss">
